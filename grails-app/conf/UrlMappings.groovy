c30f34442dd4be56c140358684bae2782309d4ea
Rahul Pratik <rahulpratik@gmail.com>
2015-08-05 21:51:46 -0700

    removed unused methods

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -3,9 +3,7 @@ class UrlMappings {
 	static mappings = {
 
         "/api/carReports"(resources: "car")
-        "/api/accident/minor"(controller: 'car', action: 'minor')
-        "/api/accident/serious"(controller: 'car', action: 'serious')
-        "/api/accident/totalled"(controller: 'car', action: 'totalled')
+       
 
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
